f174c922f3852f36ffb5f4049c6d879334175ae0
Matteo <gnappo.1@live.it>
2020-11-10 16:49:17 -0800

    Add code to identify self

--- example_1.rb
+++ example_1.rb
@@ -1,8 +1,11 @@
-
+puts "first self outside of the class"
+puts self #main:Object
 class Human
 
     ###### SELF REFERENCE 1 (LO 1) #######
-    puts self  
+    puts "second self inside of the class"
+    puts self 
+    #this is the entire class, since self has been used outside of any methods but inside the class itself
     
 
     ####### SELF REFERENCE 2 (LO 1, 3)########
@@ -10,11 +13,14 @@ class Human
     #################################
 
         ###### SELF REFERENCE 2 (LO 1)#######
-        puts self  
+        puts "third self inside of the class and inside a class method"
+
+        puts self
+        #self will be Class since we're inside a class method
         ################################
 
         ##### SELF REFERENCE 3 (LO 1)##########
-        
+        self.new(hash[:name], hash[:age])
         ##################################
     end
 
@@ -23,14 +29,20 @@ class Human
     def initialize(name, age)
         @name, @age = name, age
         ##### SELF REFERENCE 4 (LO 1)##########
-        puts self 
+        puts "fourth self inside of the class and inside an instance method"
+        puts self
+        # self will be the instance the method operates on
         #########################################
     end
 
     def introduce
         ##### SELF REFERENCE 5 (LO 1, 2)##########
-
+        puts "Hi, my name is #{self.name} and my age is #{self.age}"
         #############################################
     end
 
 end
+
+cernan = Human.new_from_hash({name: "Cernan", age: 29})
+matteo = Human.new("Matteo", 29)
+matteo.introduce
--- example_2.rb
+++ example_2.rb
@@ -4,16 +4,17 @@ class Human
     attr_accessor :name
     ##########################################
 
-
     def initialize(name)
         @name = name
     end
-
+    def self.class_method 
+        p "class method"
+    end
 
     def introduce
         ####### IMPLICIT SELF CATCH 2 ############
         puts "Hello, I am #{name}"
-        
+        self.class.class_method
         ###########################################
     end
 
@@ -21,17 +22,16 @@ class Human
     def introduce_and_ask_for_introduction
         ####### IMPLICIT SELF CATCH 2 ############
         introduce
-       
+        
         ##########################################
         puts "And you are...?"
     end
 
 
     def you_cant_confuse_me
-        introduce = 3
         introduce 
         self.introduce
-        
     end
 
 end
+Human.new("Matteo").you_cant_confuse_me
